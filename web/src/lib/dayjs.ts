a003c3c5c622dd1fe4b613a53c010a878ceb2805
Júlia Soares <109935265+juliasvgomes@users.noreply.github.com>
2025-11-11 00:10:00 -0300

    Delete web directory

--- web/src/lib/dayjs.ts
+++
@@ -1,8 +0,0 @@
-import lib from 'dayjs'
-import 'dayjs/locale/pt-BR'
-import relativeTime from 'dayjs/plugin/relativeTime'
-
-lib.locale('pt-BR')
-lib.extend(relativeTime)
-
-export const dayjs = lib
